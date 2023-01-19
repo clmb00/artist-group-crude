349b14d5cca19186c1d65c8646b7c2ca465db0e5
FR_Jess <fr_jess@icloud.com>
2023-01-19 15:59:31 +0100

    [fix] rms name

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Artisan extends Model
+class Artwork extends Model
 {
     use HasFactory;
 }
