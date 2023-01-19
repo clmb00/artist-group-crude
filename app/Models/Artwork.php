ad3b56b6a11b2a0570deb60f1a8250228b618f67
FR_Jess <fr_jess@icloud.com>
2023-01-19 16:10:10 +0100

    [add] slug

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Artwork extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($string){
+        $slug = Str::slug($string, '-');
+
+        $original_slug = $slug;
+        $c = 1;
+        $exists = Artwork::where('slug', $slug)
+        ->first();
+        while($exists){
+            $slug = $original_slug . '-' . $c;
+            $exists = Artwork::where('slug',$slug)
+            ->first();
+            $c++;
+        }
+        return $slug;
+    }
 }
